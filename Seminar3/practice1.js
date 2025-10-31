5367778ebfa2c233aac1d55ea98b808b6b3d64b0
NeaguAndrei23 <neaguandrei23@stud.ase.ro>
2025-10-31 14:00:30 +0200

    Lab 3 practice exercises

---
+++ Seminar3/practice1.js
@@ -0,0 +1,27 @@
+const dogs = [
+  {
+    name: 'Billy',
+    isDachshund: false
+  },
+  {
+    name: 'Bobby',
+    isDachshund: true
+  },
+  {
+    name: 'Mark',
+    isDachshund: true
+  }
+];
+
+const filteredDogs = dogs.filter(dog => dog.isDachshund === true);
+
+console.log(dogs);
+console.log(filteredDogs);
+
+
+/* expected output:
+[
+  {name: 'Bobby', isDachshund: true},
+  {name: 'Mark', isDachshund: true}
+]
+*/
